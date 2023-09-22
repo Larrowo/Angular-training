43741e77c803bee7daa81387309e80691f959709
Julián Astudillo <102617612+Larrowo@users.noreply.github.com>
2023-09-22 07:34:39 -0300

    New component added

--- src/app/question-form/question-form.component.ts
+++ src/app/question-form/question-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-question-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './question-form.component.html',
   styleUrls: ['./question-form.component.css']
 })
